a48ce5e818baaf4c730c6ef319cc950fcd11843a
matt <mpinder@gmail.com>
2020-08-18 21:31:53 +0100

    now using esp deep sleep functionality rather than loop and delay

--- ESP32_MQTT_HTU21D.ino
+++ ESP32_MQTT_HTU21D.ino
@@ -1,58 +1,83 @@
-#include "WiFi.h"
+#include <WiFi.h>
 #include <Wire.h>
-#include "SparkFunHTU21D.h"
+#include <SparkFunHTU21D.h>
 #include <PubSubClient.h>
 #include "device_config.h"
+
+const long uS_TO_S_FACTOR = 1000000;
 
 const String TOPIC_TEMP =  TOPIC_BASE + "/" + CLIENT_NAME + "/" + "temperature";
 const String TOPIC_HUMD =  TOPIC_BASE + "/" + CLIENT_NAME + "/" + "humidity";
 
-HTU21D myHumidity;
+HTU21D sensor;
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsgTime = 0;
-char buffer[10];
 
-void wifi_setup()
+void wifiConnect()
 {
-  WiFi.begin(ssid, password);
- 
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to WiFi..");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi...");
+    delay(1000);
+    Serial.print(".");
   }
-  Serial.print("Connected! Local IP address is: ");
+  Serial.println("connected");
+  Serial.print("IP address is: ");
   Serial.println(WiFi.localIP());
 }
 
-void reconnect()
+void mqttConnect()
 {
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = CLIENT_NAME + "-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    Serial.print("Connecting to MQTT broker...");
+    
+    if (client.connect(CLIENT_NAME.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      //client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      //client.subscribe("inTopic");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
+  }
+}
+
+void publishSensorValues()
+{
+  // Read sensor values
+  sensor.begin();
+  float humd = sensor.readHumidity();
+  float temp = sensor.readTemperature();
+
+  Serial.print("Publishing: ");
+  
+  if(temp >= -100.0 && temp <= 100.0)
+  {
+    // some basic validation
+    Serial.print(temp, 1);
+    Serial.print("C ");
+
+    // Publish to MQTT
+    client.publish(TOPIC_TEMP.c_str(), String(temp).c_str());
+  }
+
+  if(humd >= 0.0 && humd <= 100.0)
+  {
+    // some basic validation
+    Serial.print(humd, 1);
+    Serial.print("%");
+    Serial.println();
+  
+    // Publish to MQTT
+    client.publish(TOPIC_HUMD.c_str(), String(humd).c_str()); 
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  randomSeed(micros());
+  delay(1000);
+  esp_sleep_enable_timer_wakeup(INTERVAL * uS_TO_S_FACTOR);
 
   // Set up some pins to supply sensor. Current low enough to not exceed max draw on GPIO
   int h_pin = 19;
@@ -62,34 +87,21 @@ void setup()
   digitalWrite(h_pin, HIGH);
   digitalWrite(l_pin, LOW);
 
-  wifi_setup();
-  client.setServer(mqtt_server, 1883);
-  //client.setCallback(callback);
+  // Connect to Wifi network
+  wifiConnect();
+  // Connect to MQTT broker
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  mqttConnect();
+
+  publishSensorValues();
   
-  myHumidity.begin();
+  Serial.println("Going back to sleep now...");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
 
 void loop()
 {
-  float humd = myHumidity.readHumidity();
-  float temp = myHumidity.readTemperature();
-
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  long now = millis();
-  if (now - lastMsgTime > INTERVAL) {
-    lastMsgTime = now;
-    Serial.print("Publishing: ");  
-    Serial.print(temp, 1);
-    Serial.print("C ");
-    Serial.print(humd, 1);
-    Serial.print("%");
-    Serial.println();
-    
-    client.publish(TOPIC_TEMP.c_str(), String(temp).c_str());
-    client.publish(TOPIC_HUMD.c_str(), String(humd).c_str());
-  }
+  // nothing to loop
 }
--- device_config_example.h
+++ device_config_example.h
@@ -1,13 +1,15 @@
 // Wifi SSID and Password
-const char* ssid = "Wifi_Name";
-const char* password =  "Wifi_Password";
+const char* WIFI_SSID = "Wifi_Name";
+const char* WIFI_PASSWORD =  "Wifi_Password";
 
 // IP address of MQTT broker to publish to
-const uint8_t mqtt_server[] = {192, 168, 0, 99};
-// Publish interval - how often to send values to MQTT broker in milliseconds
-const long INTERVAL = 30000;
+const uint8_t MQTT_SERVER_IP[] = {192, 168, 0, 99};
+const int MQTT_SERVER_PORT = 1883;
 
 // MQTT topic information
 const String CLIENT_NAME =  "ESP32_kitchen";
 const String TOPIC_BASE =  "home";
 // resulting MQTT topic will be like <TOPIC_BASE>/<CLIENT_NAME>/<sensor> so e.g. home/ESP32_kitchen/temperature
+
+// Publish interval - how often to send values to MQTT broker in seconds
+const int INTERVAL = 30;
